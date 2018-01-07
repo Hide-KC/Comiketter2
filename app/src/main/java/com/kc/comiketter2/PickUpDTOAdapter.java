cf8a0b30a3af240a007eab6e5072a95dfde7ce16
HIDE <hide.1222.kc@gmail.com>
2018-01-07 21:05:42 +0900

    レイアウト微調整 PickupListスペース部分の修正

--- app/src/main/java/com/kc/comiketter2/PickUpDTOAdapter.java
+++ app/src/main/java/com/kc/comiketter2/PickUpDTOAdapter.java
@@ -5,6 +5,7 @@ import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.graphics.Color;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;

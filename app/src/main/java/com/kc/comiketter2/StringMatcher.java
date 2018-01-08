e43179b0228abdb58214f0bced45bc0709bfd84e
HIDE <hide.1222.kc@gmail.com>
2018-01-08 15:04:17 +0900

    StringMatcher コメント追加

--- app/src/main/java/com/kc/comiketter2/StringMatcher.java
+++ app/src/main/java/com/kc/comiketter2/StringMatcher.java
@@ -17,9 +17,9 @@ import twitter4j.Status;
 
 //サークル配置の正規表現マッチング
 public class StringMatcher {
-    //配置表示パターン。語尾abは無い場合が多い
+    //配置表示パターン。語尾abは無い場合が多い。後ろから探索
     final private static String EVENT_SPACE_PATTERN = ".*([a-zA-ZＡ-Ｚあ-んア-ン]).?([0-9０-９][0-9０-９])";
-    final private static String AB = ".*(ab)";
+    final private static String AB = ".*(ab)"; //abを探索→無ければa|bで探索
     final private static String AOrB = ".*(a|b)";
 
     //ホールHashMap

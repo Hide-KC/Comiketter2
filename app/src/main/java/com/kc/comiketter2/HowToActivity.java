3f209f028fec1c68672cf41561ba05aca7d016d1
HIDE <hide.1222.kc@gmail.com>
2018-06-11 00:13:35 +0900

    認証画面微調整 使い方画面修正済み

--- app/src/main/java/com/kc/comiketter2/HowToActivity.java
+++ app/src/main/java/com/kc/comiketter2/HowToActivity.java
@@ -1,10 +1,13 @@
 package com.kc.comiketter2;
 
 import android.app.Application;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
 import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
 public class HowToActivity extends AppCompatActivity {
 
@@ -20,5 +23,12 @@ public class HowToActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        TextView tv = findViewById(R.id.how_to_text);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N){
+            tv.setText(Html.fromHtml(getString(R.string.how_to_text), Html.FROM_HTML_MODE_LEGACY));
+        } else {
+            tv.setText(Html.fromHtml(getString(R.string.how_to_text)));
+        }
     }
 }

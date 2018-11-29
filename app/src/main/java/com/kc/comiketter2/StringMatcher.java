4d5276e4f053e11dfd3d2c712d666463a4ab6474
Hide-KC <kcs.dev.labo@gmail.com>
2018-11-29 21:03:10 +0900

    C95向け日程修正
    (cherry picked from commit 83ea375)

--- app/src/main/java/com/kc/comiketter2/StringMatcher.java
+++ app/src/main/java/com/kc/comiketter2/StringMatcher.java
@@ -76,9 +76,9 @@ public class StringMatcher {
     };
 
     //どのパターンが何日目にマッチするのかをここで定義。
-    final private static String firstDay = "([1１一]日目|金曜?日?|初日)" + "|(10|１０)日";
-    final private static String secondDay = "([2２二]日目|土曜?日?)" + "|(11|１１)日";
-    final private static String thirdDay = "([3３三]日目|日曜?日?)" + "|(12|１２)日";
+    final private static String firstDay = "([1１一]日目|土曜?日?|初日)" + "|(29|２９)日";
+    final private static String secondDay = "([2２二]日目|日曜?日?)" + "|(30|３０)日";
+    final private static String thirdDay = "([3３三]日目|月曜?日?)" + "|(31|３１)日";
 
 
     //イベント名サーチメソッドフィルタ対応版
